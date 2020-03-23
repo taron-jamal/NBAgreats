f29d07101703d83fcb44361208548f9eab13e109
taron-jamal <61537513+taron-jamal@users.noreply.github.com>
2020-03-23 02:59:52 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// Using this array:
+    // var NBAgreats = ["Kareem Abdul-Jabbar", "Julius Irving", "Magic Johnson", "Michael Jordan", "Kobe Bryant"];
+
+// 1. Remove "Kareem Abdul Jabbar" from the array.
+    array.shift();
+
+// 2. Sort the array in order.
+    array.sort();
+
+// 3. Put "LeBron James" at the end of the array.
+    array.push("LeBron James");
+
+// 4. Remove "Julius Irving" from the array.
+    array.shift();
+
+// 5. Sort the array in reverse order. (Not alphabetical, but reverse
+    array.reverse();
